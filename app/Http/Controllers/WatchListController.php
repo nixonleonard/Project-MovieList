9e1348a99233f8032bbe8628786454530b45b1cf
nixonleonard <nixonchww@gmail.com>
2023-01-06 19:00:32 +0700

    last commit paginate and movie seed

--- app/Http/Controllers/WatchListController.php
+++ app/Http/Controllers/WatchListController.php
@@ -11,27 +11,20 @@ use Illuminate\Support\Facades\DB;
 class WatchListController extends Controller
 {
     public function search(Request $request){
-        // dd($request->status);
         $search = $request->query('search');
-        // $titles = Movie::where('title', 'LIKE', "%$search%")->paginate(5)->appends(['search' => $search]);
         if($search){
             $movie = DB::table('watch_lists')->where('user_id', Auth::user()->id)->where('title', 'LIKE', "%$request->search%")->join('movies', 'movie_id', '=', 'movies.id')->paginate(4)->appends(['search' => $search]);
-            // $movie = WatchList::with('movies')->where('user_id', Auth::user()->id)->where('title', 'LIKE', "%$request->search%")->join('movies', 'movie_id', '=', 'movies.id')->paginate(4);
         }else if($request->status){
             if($request->status=='filter'){
-                $movie = DB::table('watch_lists')->where('user_id', Auth::user()->id)->join('movies', 'watch_lists.movie_id', '=', 'movies.id')->paginate(4);// $movie = WatchList::with('movies')->where('user_id', Auth::user()->id)->where('status', '=', 'Planning')->join('movies', 'movie_id', '=', 'movies.id')->paginate(4);
+                $movie = DB::table('watch_lists')->where('user_id', Auth::user()->id)->join('movies', 'watch_lists.movie_id', '=', 'movies.id')->paginate(4);
             }elseif($request->status=='all'){
                 $movie = DB::table('watch_lists')->where('user_id', Auth::user()->id)->join('movies', 'watch_lists.movie_id', '=', 'movies.id')->paginate(4);
-                // $movie = WatchList::with('movies')->where('user_id', Auth::user()->id)->join('movies', 'movie_id', '=', 'movies.id')->paginate(4);
             }elseif($request->status=='planned'){
                 $movie = DB::table('watch_lists')->where('user_id', Auth::user()->id)->where('status', '=', 'Planning')->join('movies', 'watch_lists.movie_id', '=', 'movies.id')->paginate(4);
-                // $movie = WatchList::with('movies')->where('user_id', Auth::user()->id)->where('status', '=', 'Planning')->join('movies', 'movie_id', '=', 'movies.id')->paginate(4);
             }elseif($request->status=='watching'){
                 $movie = DB::table('watch_lists')->where('user_id', Auth::user()->id)->where('status', '=', 'Watching')->join('movies', 'watch_lists.movie_id', '=', 'movies.id')->paginate(4);
-                // $movie = WatchList::with('movies')->where('user_id', Auth::user()->id)->where('status', '=', 'Watching')->join('movies', 'movie_id', '=', 'movies.id')->paginate(4);
             }elseif($request->status=='finished'){
                 $movie = DB::table('watch_lists')->where('user_id', Auth::user()->id)->where('status', '=', 'Finished')->join('movies', 'watch_lists.movie_id', '=', 'movies.id')->paginate(4);
-                // $movie = WatchList::with('movies')->where('user_id', Auth::user()->id)->where('status', '=', 'Finished')->join('movies', 'movie_id', '=', 'movies.id')->paginate(4);
             }
         }else{
             $movie = WatchList::where('user_id', Auth::user()->id)->leftJoin('movies', 'watch_lists.movie_id', '=', 'movies.id')->paginate(4);
@@ -44,14 +37,14 @@ class WatchListController extends Controller
         $valid = WatchList::where('movie_id','=',$ids)->get();
         // dd($valid);
         if(count($valid)!=0){
-            $movie = DB::table('watch_lists')->join('movies','movies.id','=','watch_lists.movie_id')->get();
+            $movie = DB::table('watch_lists')->join('movies','movies.id','=','watch_lists.movie_id')->paginate(4);
         }else{
             WatchList::create([
                 'user_id' => Auth::user()->id,
                 'movie_id' => $request->movie_id,
                 'status' => 'Planning'
             ]);
-            $movie = DB::table('watch_lists')->join('movies','movies.id','=','watch_lists.movie_id')->get();
+            $movie = DB::table('watch_lists')->join('movies','movies.id','=','watch_lists.movie_id')->paginate(4);
         }
         return view('myWatchList')->with(compact('movie'));
     }
@@ -73,7 +66,7 @@ class WatchListController extends Controller
             WatchList::where('id_wl','=',$wl_id)->where('user_id','=',Auth::user()->id)->delete();
         }
 
-        $movie = DB::table('movies')->join('watch_lists','movies.id','=','watch_lists.movie_id')->get();
+        $movie = DB::table('movies')->join('watch_lists','movies.id','=','watch_lists.movie_id')->paginate(4);
 
         return view('myWatchList')->with(compact('movie'));
     }
